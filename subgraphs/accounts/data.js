d4b9e3aa2f62cdc26d945be0c65f3ac4f368980e
Matthew Ratzke <matt@degree9.io>
2022-12-01 13:45:58 -0800

    Squashed commit of the following:
    commit 8991984db89d86bf387967aeb24c6525badbd36b
    Author: Matthew Ratzke <matt@degree9.io>
    Date:   Thu Dec 1 13:45:35 2022 -0800
    
        wip draft subgraphs with schema

---
+++ subgraphs/accounts/data.js
@@ -0,0 +1,10 @@
+export const ACCOUNTS = [
+  {
+    id: "account:1",
+    name: "Account One"
+  },
+  {
+    id: "account:2",
+    name: "Account Two"
+  },
+];
---
+++ subgraphs/transactions/data.js
@@ -0,0 +1,10 @@
+export const TRANSACTIONS = [
+  {
+    id: "transaction:1",
+    name: "Transaction One"
+  },
+  {
+    id: "transaction:2",
+    name: "Transaction Two"
+  },
+];
