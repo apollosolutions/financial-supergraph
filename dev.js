968cda72fbe429e2d3f1a85ec982563813b7a366
Shane Myrick <shane@apollographql.com>
2023-01-10 11:24:09 -0800

    Migrate subgraphs to monolith deployment

--- dev.js
+++ dev.js
@@ -1,43 +1,13 @@
-import { start as accounts } from "@apollosolutions/financial-supergraph-accounts/server.js";
-import { start as credit } from "@apollosolutions/financial-supergraph-credit/server.js";
-import { start as transactions } from "@apollosolutions/financial-supergraph-transactions/server.js";
-import { start as users } from "@apollosolutions/financial-supergraph-users/server.js";
-import { start as gateway } from "@apollosolutions/financial-supergraph-gateway/server.js";
-
-const LOCAL_SUBGRAPH_CONFIG = [
-  {
-    name: "accounts",
-    port: 4001
-  },
-  {
-    name: "credit",
-    port: 4002
-  },
-  {
-    name: "transactions",
-    port: 4003
-  },
-  {
-    name: "users",
-    port: 4004
-  }
-];
-
-const getLocalPort = (subgraphName) =>
-  LOCAL_SUBGRAPH_CONFIG.find(it => it.name === subgraphName).port;
+import { start as gateway } from "./gateway/server.js";
+import { LOCAL_SUBGRAPH_CONFIG, startSubgraphs } from "./subgraphs.js";
 
 (async () => {
-  // start all subgraphs
-  await Promise.all([
-    accounts(getLocalPort('accounts')),
-    credit(getLocalPort('credit')),
-    transactions(getLocalPort('transactions')),
-    users(getLocalPort('users')),
-  ]);
+  // start subgraphs in monolith mode
+  await startSubgraphs(4001);
 
   // wait 1s, needed for Stackblitz to load
   await new Promise((r) => setTimeout(r, 1000));
 
   // start gateway
-  await gateway(4000, LOCAL_SUBGRAPH_CONFIG);
+  await gateway(4000, 4001, LOCAL_SUBGRAPH_CONFIG);
 })();
